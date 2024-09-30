3e727db70ac8ff861ba76757b7528a3d1d5b379f
MendelCohen770 <mendeldinacohen@gmail.com>
2024-09-30 12:58:59 +0300

    search-users

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -4,30 +4,32 @@ import User from '../models/user.schema'; // Ensure this import matches your pro
 
 const getAllUsers = async (req: Request, res: Response): Promise<void> => {
   try {
-    const users = await User.find();   
+    const users = await User.find();
     res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ message: error });
   }
 };
-const createUser = async (req: Request, res: Response)=> {
+
+// פונקציה ליצירת משתמש חדש
+const createUser = async (req: Request, res: Response) => {
   const { firstName, lastName, email, phone, password, icon } = req.body;
-  // Check if all fields are provided 
+  // בודק שכל השדות מלאים 
   if (!firstName || !lastName || !email || !phone || !password) {
     res.status(400).json({ message: 'Please provide all the required fields.' });
     return;
   }
   try {
-    // Check if the user already exists
+    // בודק אם המשתמש כבר קיים
     const user = await User.findOne({ email });
     if (user) {
       res.status(400).json({ message: 'User already exists.' });
       return;
     }
-    // Create new user
-    // Hash the password
+    // יצירת משתמש חדש
+    // הצפנה של הקוד
     let newPassword: string = await bcrypt.hash(password, 10);
-    console.log(newPassword);
+    
     let newUser = new User({ firstName, lastName, email, phone, password: newPassword, icon });
     await newUser.save();
     newUser.password = '*****';
@@ -37,7 +39,69 @@ const createUser = async (req: Request, res: Response)=> {
   }
 };
 
-export { getAllUsers, createUser };
+
+// פונקציה לחיפוש משתמש
+const searchUsers = async (req: Request, res: Response) => {
+  try {
+    const { firstName, lastName, email, phone } = req.body;
+    // בודק אם השם פרטי לא NULL
+    if (firstName) {
+      const regex = new RegExp(firstName, 'i');
+      const users = await User.find({ firstName:  regex});
+
+      if (users.length > 0) {
+        res.status(200).json(users);
+        return;
+      }
+      res.status(404).json({ message: 'The user/s does not exist'});
+      return;
+    } 
+    // בודק אם השם משפחה לא NULL
+    if (lastName) {
+      const regex = new RegExp(lastName, 'i');
+      const users = await User.find({ lastName: regex });
+
+      if(users.length > 0){
+        res.status(200).json(users);
+        return;
+      }
+      res.status(404).json({ message: 'The user/s does not exist'});
+      return;
+    }
+    // בודק אם המייל לא NULL
+    if (email) {
+      const regex = new RegExp(email, 'i');
+      const users = await User.find({ email: regex });
+
+      if(users.length > 0){
+      res.status(200).json(users);
+      return;
+      }
+      res.status(404).json({ message: 'The user/s does not exist'});
+      return;
+    }
+    // בודק אם הפלאפון לא NULL
+    if (phone) {
+      const regex = new RegExp(phone, 'i');
+      const users = await User.find({ phone: regex });
+
+      if(users.length > 0){
+      res.status(200).json(users);
+      return;
+      }
+      res.status(404).json({ message: 'The user/s does not exist'});
+      return;
+    }
+    // אם כל השדות היו NULL
+      res.status(400).json({ message: 'Please provide at least one search criteria.' });
+      
+    // למקרה ויש ERROR
+  } catch (err) {
+    res.status(500).json({ message: 'Searching user/s', error: err });
+  }
+};
+
+export { getAllUsers, createUser, searchUsers };
 
 
 
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { getAllUsers, createUser } from '../controllers/user.controllers';
+import { getAllUsers, createUser, searchUsers } from '../controllers/user.controllers';
 
 const userRouter = Router();
 
 // Route to get all users
 userRouter.get('/getAllUsers', getAllUsers);
 userRouter.post('/createUser', createUser);
+userRouter.get('/searchUser', searchUsers);
 
 export default userRouter;
