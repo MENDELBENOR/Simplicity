361aa22b6e61c727ed4646a9f7ff901f20d15369
עמית <myt@h-MacBook-Air-sl-myt.local>
2024-09-30 12:18:41 +0300

    updetUser api

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -37,7 +37,55 @@ const createUser = async (req: Request, res: Response)=> {
   }
 };
 
-export { getAllUsers, createUser };
+const updateUser = async (req: Request, res: Response) => {
+  const { firstName, lastName, phone, _id } = req.body;
+  
+  // בדיקת שדות שהתקבלו
+  if (!firstName || !lastName || !phone) {
+    res.status(400).json({ message: 'Please provide all the required fields.' });
+    return;
+  }
+
+  // בדיקת אורך השם הפרטי והמשפחה
+  if (firstName.length < 2 || lastName.length < 2) {
+    res.status(400).json({ message: 'First name and last name must be at least 2 characters long.' });
+    return;
+  }
+
+  // בדיקת אורך הטלפון (נניח שבישראל זה 10 ספרות)
+const phoneRegex = /^[0-9+\-]{9,14}$/;
+  if (!phoneRegex.test(phone)) {
+    res.status(400).json({ message: 'Phone number must be between 9 and 14 digits and can include + or - at the beginning.' });
+    return;
+  }
+
+  try {
+    // חיפוש המשתמש לפי ה-ID
+    const user = await User.findById(_id);
+    if (!user) {
+      res.status(404).json({ message: 'User not found.' });
+      return;
+    }
+
+    // עדכון שם פרטי, שם משפחה וטלפון
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.phone = phone;
+
+    // שמירת השינויים
+    await user.save();
+
+    // הסתרת הסיסמה בתגובה
+    user.password = '*****';
+
+    // החזרת המשתמש המעודכן בתגובה
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ message: 'Error updating user', error: err });
+  }
+};
+
+export { getAllUsers, createUser, updateUser };
 
 
 
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { getAllUsers, createUser } from '../controllers/user.controllers';
+import { getAllUsers, createUser, updateUser } from '../controllers/user.controllers';
 
 const userRouter = Router();
 
 // Route to get all users
 userRouter.get('/getAllUsers', getAllUsers);
 userRouter.post('/createUser', createUser);
+userRouter.patch('/updateUser', updateUser);
 
 export default userRouter;
