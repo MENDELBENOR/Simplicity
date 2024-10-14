9fa747a41e977f64661b79d31530fa5127a3e697
MendelCohen770 <mendeldinacohen@gmail.com>
2024-10-14 14:14:04 +0300

    create group

--- src/controllers/group.controller.ts
+++ src/controllers/group.controller.ts
@@ -31,7 +31,31 @@ const getAllGroups = async (req: Request, res: Response) => {
         );
         res.status(500).json(response);
     }
-}
+};
 
-export { getAllGroups }
+const createGroup = async (req: Request, res: Response) => {
+    const {name, description, projectId} = req.body;
+    if (!name || !description || !projectId) {
+        const response = buildResponse(false, 'Please provide all the required fields', null, 'One of the fields (or more) is missing', null);
+        res.status(400).json(response);
+        return;
+    };
+    try{
+        const existsGroup = await Group.findOne({name});
+        if(existsGroup){
+            const response = buildResponse(false, 'Group already exists', null, null, null);
+            res.status(400).json(response);
+            return;
+        }
+        const newGroup = new Group({name, description, projectId});
+        await newGroup.save();
+        const response = buildResponse(true, 'Group created successfully', null, null, newGroup);
+        res.status(201).json(response);
+    }catch(error){
+        const response = buildResponse(false, 'Failed to create group', null,error instanceof Error ? error.message : 'Unknown error' , null);
+        res.status(500).json(response);
+    }
+};
+
+export { getAllGroups, createGroup }
 
--- src/routes/group.route.ts
+++ src/routes/group.route.ts
@@ -1,5 +1,9 @@
 import express from "express";
+import { getAllGroups, createGroup } from '../controllers/group.controller'
 
 const groupRouter = express.Router();
+
+groupRouter.get('/getAllGroups', getAllGroups);
+groupRouter.post('/createGroup', createGroup);
 
 export default groupRouter;
