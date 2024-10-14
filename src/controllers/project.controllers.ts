9da6fb9fda56c5fe678747cf19f5dd30047ce783
david <025860ds@gmail.com>
2024-10-14 10:27:42 +0300

    new function for Create project

--- src/controllers/project.controllers.ts
+++ src/controllers/project.controllers.ts
@@ -9,29 +9,29 @@ import Project from '../models/Project.schema';
 const createProject = async (req: Request, res: Response) => {
     const { name, description, icon } = req.body;
 
-    // checks that all fields are complete 
-    if (!name || !description) {
+    // checks that field is complete 
+    if (!name) {
         const response = buildResponse(
             false, 'Please provide all the required fields', null, 'One of the fields (or more) is missing', null);
-        res.status(400).json(response);
-        return;
+       res.status(400).json(response);
+       return;
     }
 
     try {
         // checks if the project exists
-        const project = await Project.findOne({ name });
-        if (project) {
+        const existingProject = await Project.findOne({ name });
+        if (existingProject) {
             const response = buildResponse(false, 'Project already exists', null, null, null);
-            res.status(400).json(response);
-            return;
+             res.status(409).json(response);
+             return;
         }
-        
+
         // Create a new project
         let newProject = new Project({ name, description, icon });
         await newProject.save();
 
         const response = buildResponse(true, 'New project successfully created', null, null, newProject)
-        res.status(200).json(response);
+        res.status(201).json(response);
 
     } catch (err) {
         const response = buildResponse(
